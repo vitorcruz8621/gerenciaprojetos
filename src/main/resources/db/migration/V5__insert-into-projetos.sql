e3f1303e03c52f4c64deb2f8a37e91d40c9fd4e0
Vitor Cruz <vitorcandrade98@gmail.com>
2024-08-14 23:05:37 -0300

    alteração de migrações, atualização de dependênicias e criaçaõ de README

--- src/main/resources/db/migration/V5__insert-into-projetos.sql
+++ src/main/resources/db/migration/V5__insert-into-projetos.sql
@@ -1,6 +1,6 @@
 INSERT INTO public.projetos (titulo, descricao, data_inicio) VALUES
-('Projeto Alpha', 'Projeto inicial para a implementação de funcionalidades básicas.', '2024-08-21'),
-('Projeto Beta', 'Projeto focado na melhoria de performance e otimização.', '2024-09-10'),
-('Projeto Gamma', 'Projeto para desenvolvimento de novos recursos avançados.', '2024-09-20'),
-('Projeto Delta', 'Projeto para integração com sistemas externos.', '2024-10-01'),
-('Projeto Epsilon', 'Projeto de manutenção e correção de bugs.', '2024-10-10');
+('Projeto JAVA', 'Descrição do Projeto JAVA', '2024-09-01'),
+('Projeto SPRING', 'Projeto SPRING', '2024-09-10'),
+('Projeto CLOUD', 'Projeto CLOUD', '2024-09-20'),
+('Projeto AWS', 'Projeto AWS', '2024-10-01'),
+('Projeto LEGADO', 'Projeto LEGADO', '2024-10-10');
